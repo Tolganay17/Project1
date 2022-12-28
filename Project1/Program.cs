eaf2c8591ee392cfc38371174f7f97c9c8ee49e4
Tolganay Muratova <tolganaimurat17@gmail.com>
2022-12-28 12:31:57 +0200

    ask user to enter username

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Nice to meet you {0}", name);
         }
     }
 }
